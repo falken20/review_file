2d523d50b00f650f193f473b2448624a39e5c2d6
Richi <r.rodriguez@bbva.com>
2022-07-11 11:13:17 +0200

    Two methods for processing files

--- README.md
+++ README.md
@@ -4,16 +4,14 @@
 <a href="https://richionline-portfolio.nw.r.appspot.com"><img src="https://falken-home.herokuapp.com/static/home_project/img/falken_logo.png" width=50 alt="Personal Portfolio web"></a>
 
 
-![Version](https://img.shields.io/badge/version-1.0.0-blue)
-[![Supported Python Versions](https://img.shields.io/pypi/pyversions/rich/10.11.0)](https://www.python.org) 
-![GitHub Top languaje](https://img.shields.io/github/languages/top/falken20/review_file) ![GitHub language count](https://img.shields.io/github/languages/count/falken20/review_file) ![Test coverage](https://img.shields.io/badge/test%20coverage-0%25-green)
+![Version](https://img.shields.io/badge/version-1.0.0-blue) ![GitHub Top languaje](https://img.shields.io/github/languages/top/falken20/review_file) ![GitHub language count](https://img.shields.io/github/languages/count/falken20/review_file) ![Test coverage](https://img.shields.io/badge/test%20coverage-0%25-green)
 
 [![Richi web](https://img.shields.io/badge/web-richionline-blue)](https://richionline-portfolio.nw.r.appspot.com) 
 [![Twitter](https://img.shields.io/twitter/follow/richionline?style=social)](https://twitter.com/richionline)
 </div>
 
 # review_file
-Script to find files in a directory tree and it has to fulfill that it has a determinate extension. The extension set is in a config file. You can configure two environment variables with the path (**PATH_DIR**) and extensions (**EXTENSIONS**) to search. If the variables doesn't exist, the script ask the user for the values.
+Script to find lines in a file with a certain pattern. You can change params inside de main file (main.py) assigning values to FILE_NAME_SOURCE, FILE_NAME_RESULT and TEXT_TO_SEARCH vars. The result file contents only the lines with the pattern.
 
 ---
 ##### Setup
@@ -25,7 +23,7 @@ pip install -r requirements.txt
 ##### Running the app
 
 ```bash
-python ./src/search_extensions.py
+python ./main.py
 ```
 
 ##### Setup tests
--- main.py
+++ main.py
@@ -1,49 +1,102 @@
+import os
+import time
+import tracemalloc
 from rich.console import Console
 from rich.progress import Progress
 
 # Create global object to manage terminal logs
 console = Console()
 
-FILE_NAME_SOURCE = "/Users/u102105/Downloads/COREJB_ACTIVIDAD_08072022.csv"
+# FILE_NAME_SOURCE = "/Users/u102105/Downloads/COREJB_ACTIVIDAD_08072022.csv"
 FILE_NAME_SOURCE = "/Users/u102105/Downloads/COREJB_ACTIVIDAD_22062022.txt" # 14 GB
 FILE_NAME_RESULT = "/Users/u102105/Downloads/final.txt"
 TEXT_TO_SEARCH = "1073741888"
+SIZE_LINE = 400  # Line size in characters
 
-def main():
 
-    console.print("Process to review lines in a file")
-
-    # FILE_NAME_SOURCE = input("File name to review: ")
-    # FILE_NAME_RESULT = input("File name to write: ")
-
-    fhandler_source = open(FILE_NAME_SOURCE, "r")
-    fhandler_result = open(FILE_NAME_RESULT, "w")
-
+def process_by_line(fhandler_source, fhandler_result):
+    """Process loading only every line
+    """
     console.print("Counting lines...")
-    lines = fhandler_source.readlines()
-    count_total_lines = len(lines)
-    console.print(f"Lines to process: {count_total_lines}")
-    
+    count_total_lines = os.path.getsize(FILE_NAME_SOURCE) / SIZE_LINE
+    console.print(f"Lines (aprox) to process: {count_total_lines:,.0f}")
+
     count_written_lines = 0
 
     with Progress() as progress:
-        task = progress.add_task(f"Searching for text: [red][bold]'{TEXT_TO_SEARCH}'[/red][/bold]...", total=count_total_lines)
-    
+        task = progress.add_task(
+            f"Searching for text: [red][bold]'{TEXT_TO_SEARCH}'[/red][/bold]...", total=count_total_lines)
+
+        for line in fhandler_source:
+            progress.update(task, advance=1)
+            if TEXT_TO_SEARCH in line:
+                fhandler_result.write(line)
+                count_written_lines += 1
+
+    console.print("\nProcess 'Using by line' finished sucessfully", style="bold green")
+    console.print(f"Lines processed: {count_total_lines:,.0f} lines", style="yellow")
+    console.print(f"Lines written: {count_written_lines:,} lines", style="yellow")
+
+
+def process_by_list(fhandler_source, fhandler_result):
+    """Process loading all the lines in memory
+    """
+    console.print("Counting lines...")
+    lines = fhandler_source.readlines()
+    count_total_lines = len(lines)
+    console.print(f"Lines to process: {count_total_lines:,}")
+
+    count_written_lines = 0
+
+    with Progress() as progress:
+        task = progress.add_task(
+            f"Searching for text: [red][bold]'{TEXT_TO_SEARCH}'[/red][/bold]...", total=count_total_lines)
+
         for line in lines:
             progress.update(task, advance=1)
             if TEXT_TO_SEARCH in line:
                 fhandler_result.write(line)
                 count_written_lines += 1
 
+    console.print("\nProcess 'Using a List' finished sucessfully", style="bold green")
+    console.print(f"Lines processed: {count_total_lines:,} lines", style="yellow")
+    console.print(f"Lines written: {count_written_lines:,} lines", style="yellow")
+
+
+def main():
+    # Start to measure memory use
+    tracemalloc.start()
+    console.print("\n***** review_file started *****\n", style="bold green")
+
+    time_init = time.time()
+
+    file_size = os.path.getsize(FILE_NAME_SOURCE) / 1024 / 1024 / 1024
+    console.print(f"File size: {file_size:,.2f} Gb")
+
+    fhandler_source = open(FILE_NAME_SOURCE, "r")
+    fhandler_result = open(FILE_NAME_RESULT, "w")
+
+    process_by_line(fhandler_source, fhandler_result)
+    # process_by_list(fhandler_source, fhandler_result)  # Use a lot of memory
+
     fhandler_source.close()
     fhandler_result.close()
 
-    console.print("Process finished sucessfully")
-    console.print(f"Lines processed: {count_total_lines}")
-    console.print(f"Lines written: {count_written_lines}")
-  
+    console.print(
+        f"Process time: {time.time() - time_init:,.2f} sg", style="yellow")
+    file_size = os.path.getsize(FILE_NAME_RESULT) / 1024 / 1024 / 1024
+    console.print(f"File result size: {file_size:,.2f} Gb", style="yellow")
+
+    # displaying the memory used
+    current, peak = tracemalloc.get_traced_memory()
+    console.print(f"Memory current size: {current:,}", style="yellow")
+    console.print(f"Memory peak size: {peak:,}", style="yellow")
+
+    # Stop memory use measure
+    tracemalloc.stop()
+    console.print(
+        "\n[bold green]***** review_file finished :smile: *****[/bold green]\n")
+
 
 if __name__ == "__main__":
-    console.print("\n***** review_file started *****\n", style="bold green")
     main()
-    console.print("\n[bold green]***** review_file finished :smile: *****[/bold green]\n")
